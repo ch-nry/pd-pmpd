fb7f7973be9a2bf3841f2f396b0adda0d5fd2cdb
chnry <ch@chnry.net>
2021-11-15 15:19:51 +0100

    ajout d'un message concernant la licence

--- pmpd3d_test.c
+++ pmpd3d_test.c
@@ -1,3 +1,20 @@
+// --------------------------------------------------------------------------
+// This file is part of the pmpd software.
+//
+//    pmpd software is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    pmpd firmware is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with pmpd software. If not, see <http://www.gnu.org/licenses/>.
+// -------------------------------------------------------------------------- 
+
 int pmpd3d_test_3d_mass(int i, t_pmpd3d *x, int argc, t_atom *argv)
 {
 	t_float tmp;
