4f0c5fcb8011bbf687646f15ed23aebf2bdb57c1
Antoine Villeret <antoine@makula.be>
2021-11-19 15:07:49 +0100

    add Dockerfile to build image for cross compilation

---
+++ cross_compile_rpi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SOURCE_FOLDER=/pmpd
+BUILD_FOLDER=/build
+
+cmake -S ${SOURCE_FOLDER} -B ${BUILD_FOLDER} -G Ninja -DCMAKE_TOOLCHAIN_FILE="${SOURCE_FOLDER}/arm-linux-gnueabihf.cmake"
+cmake --build ${BUILD_FOLDER}
+cmake --build ${BUILD_FOLDER} --target install
