59a4c76b7eb1fef8931e3c5aa9876896b43aedbf
Antoine Villeret <antoine@makula.be>
2021-11-19 19:07:16 +0100

    try to target armv6

--- cross_compile_rpi.sh
+++ cross_compile_rpi.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SOURCE_FOLDER=${SCRIPT_DIR}
 BUILD_FOLDER=${SOURCE_FOLDER}/build
 
-cmake -S ${SOURCE_FOLDER} -B ${BUILD_FOLDER} -G Ninja -DCMAKE_TOOLCHAIN_FILE="${SOURCE_FOLDER}/arm-linux-gnueabihf.cmake"
+cmake -S ${SOURCE_FOLDER} -B ${BUILD_FOLDER} -G Ninja -DCMAKE_TOOLCHAIN_FILE="${SOURCE_FOLDER}/arm-linux-gnueabihf.cmake" -DCMAKE_C_FLAGS="-marm"
 cmake --build ${BUILD_FOLDER}
 cmake --build ${BUILD_FOLDER} --target install
