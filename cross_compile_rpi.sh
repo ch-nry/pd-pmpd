48a9279245d79c75720ebfb77c0888a538e54a43
Antoine Villeret <antoine@makula.be>
2021-11-19 14:44:15 +0000

    fix cross building in docker

--- cross_compile_rpi.sh
+++ cross_compile_rpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_FOLDER=/pmpd
-BUILD_FOLDER=/build
+BUILD_FOLDER=${SOURCE_FOLDER}/build
 
 cmake -S ${SOURCE_FOLDER} -B ${BUILD_FOLDER} -G Ninja -DCMAKE_TOOLCHAIN_FILE="${SOURCE_FOLDER}/arm-linux-gnueabihf.cmake"
 cmake --build ${BUILD_FOLDER}
